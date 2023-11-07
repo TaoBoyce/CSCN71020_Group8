79b4388e0099bffd6a1599fb1461180bec38e71a
TaoBoyce <tao.z.boyce@gmail.com>
2023-11-06 19:22:13 -0500

    added ReadInt_ZeroValid, filled getFourPoints

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #include "main.h"
@@ -22,16 +24,29 @@ int main() {
 			//printf_s("! %d\n", triangleSidesPtr[0]);
 			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
 			printf_s("%s\n", result);
-			//if it's a triangle, print out the angles
+			if (result != NOT_TRIANGLE) {
+				//print angles
+			}
 			break;
 		case 2:
 			printf_s("Rectangle selected.\n");
+			//[0] is X
+			//[1] is Y
 			int rectanglePoints[RECT_COORD_WIDTH][RECT_COORD_COUNT] = {0};
 			bool gotPoints = getFourPoints(rectanglePoints);
 			if (!gotPoints) {
 				printf_s("Invalid value entered.\n");
-				return 1;
+				break;
 			}
+			//char* result = analyzeRectangle(rectanglePoints);
+			/*
+			if(result = RECTANGLE){
+				//get area
+				//print area
+				//get perimeter
+				//print perimeter
+			}
+			*/
 			break;
 		case 0:
 			continueProgram = false;
@@ -60,7 +75,7 @@ int printShapeMenu() {
 	int shapeChoice;
 
 	printf_s("Enter number: ");
-	scanf_s("%1o", &shapeChoice);
+	ReadInt_ZeroValid(&shapeChoice);
 
 	return shapeChoice;
 }
@@ -69,12 +84,51 @@ int* getTriangleSides(int* triangleSides) {
 	printf_s("Enter the three sides of the triangle: ");
 	for (int i = 0; i < 3; i++)
 	{
-		scanf_s("%d", &triangleSides[i]);
+		while (!ReadInt_ZeroValid(&triangleSides[i])) {
+			printf_s("Invalid input, try again.");
+		}
 	}
 	return triangleSides;
 }
 
 bool getFourPoints(int rectanglePoints[RECT_COORD_WIDTH][RECT_COORD_COUNT]) {
-	printf_s("Enter the four points of a rectangle: ");
+	printf_s("Enter the four points of a rectangle: \n");
+	//asks for each pair of coordinates
+	for (int col = 0; col < RECT_COORD_COUNT; col++) {
+		for (int row = 0; row < RECT_COORD_WIDTH; row++) {
+
+			do {
+				if (row % RECT_COORD_WIDTH == 0) {
+					printf_s("Point %d:\nX: ", row + 1 - row / 2);
+				}
+				else {
+					printf_s("Y: ");
+				}
+			} while (!ReadInt_ZeroValid(&rectanglePoints[row][col]));
+			//testing
+			//printf_s("Row: %d, Column: %d\n", row, col);
+			//printf_s("Value: %d\n", rectanglePoints[row][col]);
+		}
+	}
+	return true;
+}
+
+bool ReadInt_ZeroValid(int* output) {
+	char buf[BUFF_SIZE];
+	if (fgets(buf, BUFF_SIZE, stdin) == NULL) {
+		return false;
+	}
+	char* endptr;
+	errno = 0;
+	long num = strtol(buf, &endptr, 10);
+	if (buf == endptr || (*endptr != '\n' && *endptr != '\0')) {
+		return false;  // failed conversion
+	}
+	if (errno || num < INT_MIN || num > INT_MAX) {
+		return false;  // range error
+	}
+	if (output != NULL) {
+		*output = (int)num;
+	}
 	return true;
 }
--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -1,11 +1,17 @@
 #pragma once
 #include <stdbool.h>
-#define RECT_COORD_WIDTH 2
-#define RECT_COORD_COUNT 4
+#define RECT_COORD_WIDTH	2
+#define RECT_COORD_COUNT	4
+#define BUFF_SIZE			256
 
 void printWelcome();
 int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
 
 //tao - fall23
+
+//gets four coordinates
 bool getFourPoints(int rectanglePoints[RECT_COORD_WIDTH][RECT_COORD_COUNT]);
+
+//parses an input, and turns it into an int, 0 is valid
+bool ReadInt_ZeroValid(int* output);
