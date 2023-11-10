7b987d5d35a17612ce55c6fe03a80b972f422adb
Kady O'grady <Kogrady1917@conestogac.on.ca>
2023-11-10 09:20:46 -0500

    Removed debug line

--- PolygonChecker/anglechecker.c
+++ PolygonChecker/anglechecker.c
@@ -23,8 +23,6 @@ bool GetTriAngles(int side1, int side2, int side3, double *out) {
 
 	double area = sqrt((side1d + side2d + side3d) * (-side1d + side2d + side3d) * (side1d - side2d + side3d) * (side1d + side2d - side3d)) / 4;
 
-	printf_s("Area: %lf \n", area);
-
 	out[0] = RAD2DEG * asin((2 * area) / (side1d * side2d));
 	out[1] = RAD2DEG * asin((2 * area) / (side2d * side3d));
 	out[2] = RAD2DEG * asin((2*area) / (side3d*side1d));
